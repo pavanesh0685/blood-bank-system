58ce5334b9f13c1bdf0cf1baa1a959e6a3c1719b
pavanesh0685 <139573586+pavanesh0685@users.noreply.github.com>
2024-05-17 10:11:09 +0530

    blood bank

---
+++ Blood-Bank/sql/thankyou.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</head>
+
+<body>
+  <?php include('head.php') ?>
+
+  <div id="page-container" style="margin-top:50px; position: relative;min-height: 84vh;">
+    <div class="container">
+      <div id="content-wrap" style="padding-bottom:50px;">
+        <div class="row">
+          <div class="col-lg-12">
+            <h1 class="mt-4 mb-3">Congratulations!</h1>
+            <p>You have successfully become a blood donor.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php include('footer.php') ?>
+
+</body>
+
+</html>
